224cb7bdc88c88e6412efe0d0515d8c1bab5dd6e
gudtldn <gudtldn1234@gmail.com>
2025-09-29 13:00:02 +0900

    feat: NGGameModeBase 클래스 추가 및 프로젝트 기본 클래스로 설정

---
+++ Source/ProjectNG/Private/NGGameModeBase.cpp
@@ -0,0 +1,4 @@
+﻿// Copyright (c) 2025 TeamNG. All Rights Reserved.
+
+
+#include "ProjectNG/Public/NGGameModeBase.h"
---
+++ Source/ProjectNG/Public/NGGameModeBase.h
@@ -0,0 +1,16 @@
+﻿// Copyright (c) 2025 TeamNG. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NGGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTNG_API ANGGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+};
