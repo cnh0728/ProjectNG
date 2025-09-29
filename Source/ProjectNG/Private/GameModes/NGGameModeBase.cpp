4ecb2a3dcb967090afe1b7540d91e3ceb47072f9
gudtldn <gudtldn1234@gmail.com>
2025-09-29 14:35:49 +0900

    feat: NGGameInstance 클래스 추가 및 프로젝트 기본 GameInstance로 설정
    - NGGameInstance 클래스 정의 및 구현 추가
    - DefaultEngine.ini에 GameInstanceClass를 NGGameInstance로 변경
    - 파일 구조 정리 (NGGameModeBase 관련 파일 경로 변경)

--- Source/ProjectNG/Private/GameModes/NGGameModeBase.cpp
+++ Source/ProjectNG/Private/GameModes/NGGameModeBase.cpp
@@ -1,4 +1,4 @@
 ﻿// Copyright (c) 2025 TeamNG. All Rights Reserved.
 
 
-#include "ProjectNG/Public/NGGameModeBase.h"
+#include "ProjectNG/Public/GameModes/NGGameModeBase.h"
---
+++ Source/ProjectNG/Public/Core/NGGameInstance.h
@@ -0,0 +1,16 @@
+﻿// Copyright (c) 2025 TeamNG. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "NGGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTNG_API UNGGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+};
