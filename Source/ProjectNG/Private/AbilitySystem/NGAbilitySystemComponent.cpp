7b2fb192aee14734be7d9de86aa3fd4f4b5fab5c
gudtldn <gudtldn1234@gmail.com>
2025-10-08 17:06:25 +0900

    feat: NGAbilitySystemComponent 클래스 추가
    - AbilitySystemComponent를 확장한 UNGAbilitySystemComponent 클래스 정의 및 구현
    - BeginPlay, TickComponent 오버라이드하여 기본 동작 설정

---
+++ Source/ProjectNG/Private/AbilitySystem/NGAbilitySystemComponent.cpp
@@ -0,0 +1,37 @@
+﻿// Copyright (c) 2025 TeamNG. All Rights Reserved.
+
+
+#include "AbilitySystem/NGAbilitySystemComponent.h"
+
+
+// Sets default values for this component's properties
+UNGAbilitySystemComponent::UNGAbilitySystemComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UNGAbilitySystemComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UNGAbilitySystemComponent::TickComponent(
+	float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction
+)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
