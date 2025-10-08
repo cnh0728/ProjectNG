0eff1b44ad7851b416fd79a32dadfb87d6f449d5
gudtldn <gudtldn1234@gmail.com>
2025-10-08 17:20:31 +0900

    feat: NGPawnBase 클래스 추가
    - APawn을 확장한 ANGPawnBase 클래스 정의 및 템플릿 코드 구현
    - IAbilitySystemInterface 구현 및 AbilitySystemComponent 초기화
    - GAS 지원을 위한 기반 Pawn 클래스 추가

---
+++ Source/ProjectNG/Public/Pawn/NGPawnBase.h
@@ -0,0 +1,27 @@
+﻿// Copyright (c) 2025 TeamNG. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
+#include "AbilitySystem/NGAbilitySystemComponent.h"
+#include "GameFramework/Pawn.h"
+#include "NGPawnBase.generated.h"
+
+UCLASS()
+class PROJECTNG_API ANGPawnBase : public APawn, public IAbilitySystemInterface
+{
+private:
+	GENERATED_BODY()
+
+public:
+	ANGPawnBase();
+
+	//~Begin IAbilitySystemInterface
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override { return AbilitySystemComponent; }
+	//~End IAbilitySystemInterface
+
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "AbilitySystemComponent")
+	TObjectPtr<UNGAbilitySystemComponent> AbilitySystemComponent;
+};
