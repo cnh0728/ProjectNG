b54cec50235645fc9ecee7049445ed1b835820d9
gudtldn <gudtldn1234@gmail.com>
2025-10-08 17:10:54 +0900

    feat: NGInputComponent 클래스 추가
    - UEnhancedInputComponent를 확장한 UNGInputComponent 클래스 정의 및 구현
    - 기본 생성자, BeginPlay, TickComponent 오버라이드 추가

---
+++ Source/ProjectNG/Public/Input/NGInputComponent.h
@@ -0,0 +1,26 @@
+﻿// Copyright (c) 2025 TeamNG. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnhancedInputComponent.h"
+#include "NGInputComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class PROJECTNG_API UNGInputComponent : public UEnhancedInputComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UNGInputComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+};
